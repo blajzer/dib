c5fcf684144333030f02991da187acdd9e2036cf
Brett Lajzer <brett@brettlajzer.com>
2011-01-06 22:11:54 -0500

    Added timestamp database functions.

--- Dib.hs
+++ Dib.hs
@@ -3,17 +3,20 @@ module Dib (SrcTransform(OneToOne, OneToMany, ManyToOne, ManyToMany),
             execAction,
             Rule,
             evalRule,
-            Action,
+            Action(Action),
             runAction
             ) where
 
 import Control.Monad
-import Data.Array
+import qualified Data.Map as M
+import qualified Data.ByteString as B
 import Data.List
 import Data.Maybe
+import Data.Serialize
 import System.Cmd (system)
 import System.Directory as D
 import System.FilePath
+import System.Time as T
 
 -- Data type for mapping between src and destination files
 data SrcTransform = OneToOne FilePath FilePath
@@ -39,20 +42,37 @@ class Rule r where
 --  and an Actionable that processes these transformations
 data (Actionable a, Rule r) => Action r a = Action r a
 
+--TODO: modify this to pass in the real map
 runAction :: (Actionable a, Rule r) => Action r a -> [FilePath] -> IO [FilePath]
-runAction (Action rule impl) files = let gatheredFiles = evalRule rule files in (filterM shouldBuildTransform gatheredFiles) >>= (execAction impl) >> (return $ extractTargets gatheredFiles)
+runAction (Action rule impl) files = let gatheredFiles = evalRule rule files in (filterM (shouldBuildTransform M.empty) gatheredFiles) >>= (execAction impl) >> (return $ extractTargets gatheredFiles)
 
---monadic Boolean operators
-mNot :: IO Bool -> IO Bool
-mNot = liftM not
-(<&&>) :: IO Bool -> IO Bool -> IO Bool
-(<&&>) = liftM2 (&&)
+(<||>) :: IO Bool -> IO Bool -> IO Bool
+(<||>) = liftM2 (||)
 
 -- function for filtering transforms based on them already being taken care of
 -- TODO: add in something that queries the database
-{-# NOINLINE shouldBuildTransform #-}
-shouldBuildTransform :: SrcTransform -> IO Bool
-shouldBuildTransform (OneToOne s d) = liftM not $ D.doesFileExist d
-shouldBuildTransform (OneToMany s ds) = liftM (not.and) $ mapM D.doesFileExist ds
-shouldBuildTransform (ManyToOne ss d) = liftM not $ D.doesFileExist d
-shouldBuildTransform (ManyToMany ss ds) = liftM (not.and) $ mapM D.doesFileExist ds
+shouldBuildTransform :: M.Map FilePath Integer -> SrcTransform -> IO Bool
+shouldBuildTransform m (OneToOne s d) = hasSrcChanged m [s] <||> (liftM not $ D.doesFileExist d)
+shouldBuildTransform m (OneToMany s ds) = hasSrcChanged m [s] <||> (liftM (not.and) $ mapM D.doesFileExist ds)
+shouldBuildTransform m (ManyToOne ss d) = hasSrcChanged m ss <||> (liftM not $ D.doesFileExist d)
+shouldBuildTransform m (ManyToMany ss ds) = hasSrcChanged m ss <||> (liftM (not.and) $ mapM D.doesFileExist ds)
+
+hasSrcChanged :: M.Map FilePath Integer -> [FilePath] -> IO Bool
+hasSrcChanged m f = let filesInMap = zip f $ map (flip M.lookup m) f
+                        checkTimeStamps _ (_, Nothing) = return True
+                        checkTimeStamps b (f, Just s) = (getTimestamp f) >>= (\t -> return $ b || (t /= s))
+                    in foldM checkTimeStamps True filesInMap
+
+getTimestamp f = do doesExist <- D.doesFileExist f
+                    if doesExist then D.getModificationTime f >>= extractSeconds else return 0
+                    where extractSeconds (TOD s p) = return s             
+
+loadDatabase :: IO (M.Map FilePath Integer)
+loadDatabase = do fileExists <- D.doesFileExist ".dibdb"
+                  fileContents <- if fileExists then B.readFile ".dibdb" else return B.empty
+                  return.handleEither $ decode fileContents
+                  where handleEither (Left _) = M.empty
+                        handleEither (Right a) = a
+                        
+writeDatabase :: (M.Map FilePath Int) -> IO ()
+writeDatabase m = do B.writeFile ".dibdb" $ encode m
