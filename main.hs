01c71c3d2cd4a7c58ba3c4b4db13469130f1a0d7
Brett Lajzer <brett@brettlajzer.com>
2011-03-04 22:55:38 -0500

    Made "dib" console command suck less.
    It will now traverse up the folder hierarchy to try to find a "dib.hs" file that it can execute, if possible.

--- main.hs
+++ main.hs
@@ -1,8 +1,18 @@
 module Main where
 
+import GHC.IO.Exception
 import System.Cmd (system)
-import System.Directory as D
+import qualified System.Directory as D
 
+findDib lastPath = do
+    curPath <- D.getCurrentDirectory
+    if curPath /= lastPath then
+        D.doesFileExist "dib.hs">>= \hasDib -> if hasDib then system "runhaskell dib.hs" else (D.setCurrentDirectory "../" >> findDib curPath)
+     else
+        putStrLn "Error: can't find dib.hs." >> return (ExitFailure 255)
 
 main = do
-    system "runhaskell dib.hs"
+    currentDir <- D.getCurrentDirectory
+    exitcode <- findDib ""
+    D.setCurrentDirectory currentDir
+    return exitcode
