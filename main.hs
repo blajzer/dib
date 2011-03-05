230db3549c1be0ee59ee3327082c5ee1660282f5
Brett Lajzer <brett@brettlajzer.com>
2011-03-05 16:21:05 -0500

    Pass arguments on to the dib.hs file from the "dib" executable.

--- main.hs
+++ main.hs
@@ -1,18 +1,21 @@
 module Main where
 
+import Data.List (intercalate)
 import GHC.IO.Exception
 import System.Cmd (system)
 import qualified System.Directory as D
+import System.Environment (getArgs)
 
-findDib lastPath = do
+findDib lastPath args = do
     curPath <- D.getCurrentDirectory
     if curPath /= lastPath then
-        D.doesFileExist "dib.hs">>= \hasDib -> if hasDib then system "runhaskell dib.hs" else (D.setCurrentDirectory "../" >> findDib curPath)
+        D.doesFileExist "dib.hs">>= \hasDib -> if hasDib then system ("runhaskell dib.hs " ++ args) else (D.setCurrentDirectory "../" >> findDib curPath args)
      else
         putStrLn "Error: can't find dib.hs." >> return (ExitFailure 255)
 
 main = do
+    args <- getArgs
     currentDir <- D.getCurrentDirectory
-    exitcode <- findDib ""
+    exitcode <- findDib "" (intercalate "" args)
     D.setCurrentDirectory currentDir
     return exitcode
