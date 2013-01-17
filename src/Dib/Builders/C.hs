cd575f960893e7c5bc56c9e7546f94ec2cfce8b2
Brett Lajzer <brett@brettlajzer.com>
2013-01-16 22:28:53 -0500

    Added remapped build directory support to the C builder.

--- src/Dib/Builders/C.hs
+++ src/Dib/Builders/C.hs
@@ -1,7 +1,9 @@
 module Dib.Builders.C (
-  CTargetInfo(CTargetInfo, projectName, srcDir, compiler, linker, inFileOption, outFileOption, compileFlags, linkFlags, includeDirs),
+  CTargetInfo(CTargetInfo, projectName, srcDir, outputLocation, compiler, linker, inFileOption, outFileOption, compileFlags, linkFlags, includeDirs, extraCompileDeps, extraLinkDeps),
+  BuildLocation(InPlace, BuildDir, ObjAndBinDirs),
   makeCTarget,
-  makeCleanTarget
+  makeCleanTarget,
+  makeBuildDirs
   ) where
 
 import Dib.Gatherers
@@ -13,10 +15,12 @@ import qualified Data.Text as T
 import System.Cmd (system)
 import System.Directory as D
 import System.Exit
+import System.FilePath as F
 
 data CTargetInfo = CTargetInfo {
   projectName :: T.Text,
   srcDir :: T.Text,
+  outputLocation :: BuildLocation,
   compiler :: String,
   linker :: String,
   inFileOption :: String,
@@ -28,6 +32,31 @@ data CTargetInfo = CTargetInfo {
   extraLinkDeps :: [T.Text]
   }
 
+data BuildLocation = InPlace
+                   | BuildDir T.Text
+                   | ObjAndBinDirs T.Text T.Text
+
+massageFilePath :: T.Text -> T.Text
+massageFilePath p = T.replace "\\" "_" $ T.replace "/" "_" p
+
+remapObjFile :: BuildLocation -> T.Text -> T.Text
+remapObjFile InPlace f = f
+remapObjFile (BuildDir d) f = d `T.snoc` F.pathSeparator `T.append` (massageFilePath f)
+remapObjFile (ObjAndBinDirs d _) f = d `T.snoc` F.pathSeparator `T.append` (massageFilePath f) 
+
+remapBinFile :: BuildLocation -> T.Text -> T.Text
+remapBinFile InPlace f = f
+remapBinFile (BuildDir d) f = d `T.snoc` F.pathSeparator `T.append` f
+remapBinFile (ObjAndBinDirs _ d) f = d `T.snoc` F.pathSeparator `T.append` f
+
+makeBuildDirs :: CTargetInfo -> IO ()
+makeBuildDirs info = do
+  let helper (InPlace) = return ()
+      helper (BuildDir d) = D.createDirectoryIfMissing True (T.unpack d)
+      helper (ObjAndBinDirs d d2) = D.createDirectoryIfMissing True (T.unpack d) >> D.createDirectoryIfMissing True (T.unpack d2)
+  helper (outputLocation info)
+  return ()
+  
 makeCTarget :: CTargetInfo -> Target
 makeCTarget info = 
   let makeBuildString s t = compiler info ++ " " ++ inFileOption info ++ " " ++ T.unpack s ++ " " ++ outFileOption info ++ " " ++ T.unpack t ++ " " ++ compileFlags info
@@ -48,13 +77,13 @@ makeCTarget info =
         handleExitCode exitCode t linkString
       linkCmd _ = return $ Right "Unhandled SrcTransform."
 
-      cppStage = Stage "compile" (map (changeExt "o")) (cDepScanner (includeDirs info) (extraCompileDeps info)) buildCmd
-      linkStage = Stage "link" (combineTransforms (projectName info) (extraLinkDeps info)) return linkCmd
+      cppStage = Stage "compile" (map (changeExt "o" (outputLocation info))) (cDepScanner (includeDirs info) (extraCompileDeps info)) buildCmd
+      linkStage = Stage "link" (combineTransforms (remapBinFile (outputLocation info) $ projectName info) (extraLinkDeps info)) return linkCmd
   in Target (projectName info) [] [cppStage, linkStage] [makeFileTreeGatherer (srcDir info) (matchExtensions [".cpp", ".c"])]
 
-changeExt :: T.Text -> SrcTransform -> SrcTransform
-changeExt newExt (OneToOne l _) = OneToOne l $ T.append (T.dropWhileEnd (/='.') l) newExt
-changeExt _ _ = undefined
+changeExt :: T.Text -> BuildLocation -> SrcTransform -> SrcTransform
+changeExt newExt b (OneToOne l _) = OneToOne l $ remapObjFile b $ T.append (T.dropWhileEnd (/='.') l) newExt
+changeExt _ _ _ = undefined
 
 handleExitCode :: ExitCode -> T.Text -> String -> IO (Either SrcTransform T.Text)
 handleExitCode (ExitSuccess) t _ = return $ Left $ OneToOne t ""
@@ -72,8 +101,16 @@ makeCleanTarget info =
         return $ Left $ OneToOne "" ""
       cleanCmd _ = error "Should never hit this."
   
+      objDir InPlace = srcDir info
+      objDir (BuildDir d) = d
+      objDir (ObjAndBinDirs d _) = d
+      
+      programFile InPlace = projectName info
+      programFile (BuildDir d) = d `T.snoc` F.pathSeparator `T.append` projectName info
+      programFile (ObjAndBinDirs _ d) = d `T.snoc` F.pathSeparator `T.append` projectName info
+  
       cleanStage = Stage "clean" id return cleanCmd
-      objectGatherer = makeFileTreeGatherer (srcDir info) (matchExtension ".o")
-      programGatherer = makeSingleFileGatherer (projectName info)
+      objectGatherer = makeFileTreeGatherer (objDir $ outputLocation info) (matchExtension ".o")
+      programGatherer = makeSingleFileGatherer (programFile $ outputLocation info)
   in Target ("clean-" `T.append` (projectName info)) [] [cleanStage] [objectGatherer, programGatherer]
 
